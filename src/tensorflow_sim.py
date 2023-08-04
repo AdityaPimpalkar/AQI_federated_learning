6924f6b8a893229c5eaf2007a4866dc5f799beb6
aditya pimpalkar <adipimpalkar@gmail.com>
2023-08-04 23:46:38 +0100

    attention layer and evaluation

--- src/tensorflow_sim.py
+++ src/tensorflow_sim.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import tensorflow as tf
 import tensorflow_federated as tff
+import keras
+import keras.backend as K
 import collections
 import time
 import math
@@ -11,14 +13,40 @@ def create_dataset(data):
     targets = data.loc[:, data.columns.isin(["pm2.5", "pm10"])]
     #creating a timeseries datase
     return tf.keras.utils.timeseries_dataset_from_array(
-        features.to_numpy(), 
-        targets.to_numpy(), 
+        features.to_numpy(),
+        targets.to_numpy(),
         #interval period, we have data at every 1 hour intervals
         sequence_length=1,
-        batch_size=1, 
-        sequence_stride=1, 
+        batch_size=1,
+        sequence_stride=1,
         shuffle=False
     )
+
+#ref - https://machinelearningmastery.com/adding-a-custom-attention-layer-to-recurrent-neural-network-in-keras/
+class Attention(keras.layers.Layer):
+    def __init__(self,**kwargs):
+        super(Attention,self).__init__(**kwargs)
+ 
+    def build(self,input_shape):
+        self.W=self.add_weight(name='attention_weight', shape=(input_shape[-1],1), 
+                               initializer='random_normal', trainable=True)
+        self.b=self.add_weight(name='attention_bias', shape=(input_shape[1],1), 
+                               initializer='zeros', trainable=True)        
+        super(Attention, self).build(input_shape)
+ 
+    def call(self,x):
+        # Alignment scores. Pass them through tanh function
+        e = K.tanh(K.dot(x,self.W)+self.b)
+        # Remove dimension of size 1
+        e = K.squeeze(e, axis=-1)   
+        # Compute the weights
+        alpha = K.softmax(e)
+        # Reshape to tensorFlow format
+        alpha = K.expand_dims(alpha, axis=-1)
+        # Compute the context vector
+        context = x * alpha
+        context = K.sum(context, axis=1)
+        return context
 
 def create_fl_clients(data, no_of_clients):
     '''
@@ -46,14 +74,15 @@ def model_fn():
         [
             tf.keras.layers.Input(shape=(1, 9)),
             tf.keras.layers.Conv1D(
-                filters=60, 
+                filters=20,
                 kernel_size=5,
-                strides=1, 
+                strides=1,
                 padding="causal",
               activation="relu"
             ),
-            tf.keras.layers.LSTM(60, return_sequences=True),
             tf.keras.layers.LSTM(30, return_sequences=True),
+            tf.keras.layers.LSTM(20, return_sequences=True),
+            Attention(),
             tf.keras.layers.Dense(30, activation="relu"),
             tf.keras.layers.Dense(10, activation="relu"),
             tf.keras.layers.Dense(2),
@@ -76,20 +105,17 @@ def train(trainer ,train_data, num_rounds, no_of_clients):
         t2 = time.time()
         print('train metrics {m}, round time {t:.2f} seconds'.format(
             m=train_metrics, t=t2 - t1))
-    
+
     return state
 
-def evaluation(state, data):
-    eval = tff.learning.build_federated_evaluation(model_fn)
-    metrics = eval(state.model, data)
-    print(metrics)
+
 
 if __name__ == "__main__":
     #setting number of clients
     NUM_CLIENTS = 20
     ROUNDS = 8
-    SERVER_LR = 0.001
-    CLIENT_LR = 0.1
+    SERVER_LR = 0.03
+    CLIENT_LR = 0.06
 
     data = pd.read_csv('dublin_port.csv')
     #dropping uncessary columns
@@ -107,16 +133,24 @@ if __name__ == "__main__":
 
     trainer = tff.learning.algorithms.build_weighted_fed_avg(
         model_fn,
-        server_optimizer_fn=lambda: tf.keras.optimizers.SGD(SERVER_LR),
+        #server_optimizer_fn=lambda: tf.keras.optimizers.SGD(SERVER_LR),
         client_optimizer_fn=lambda: tf.keras.optimizers.SGD(CLIENT_LR)
     )
 
     state = train(
         trainer,
-        train_data, 
-        num_rounds=ROUNDS, 
+        train_data,
+        num_rounds=ROUNDS,
         no_of_clients=NUM_CLIENTS
     )
 
-    evaluation(state, val_data)
-    evaluation(state, test_data)
+    eval = tff.learning.build_federated_evaluation(model_fn)
+
+    eval_client_data = create_dataset(val_data)
+    metrics = eval(trainer.get_model_weights(state), [eval_client_data])
+    print("Eval set metrics" ,metrics)
+
+    test_client_data = create_dataset(test_data)
+    metrics = eval(trainer.get_model_weights(state), [test_client_data])
+    print("Test set metrics" ,metrics)
+
